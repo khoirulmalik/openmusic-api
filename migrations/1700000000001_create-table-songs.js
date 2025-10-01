65651eba3fdc7542247d3c6cab1fcd05b45a2d29
Khoirul Malik <khoirulmalik03@gmail.com>
2025-10-01 12:01:00 +0700

    feat: albums & songs CRUD

---
+++ migrations/1700000000001_create-table-songs.js
@@ -0,0 +1,43 @@
+exports.up = (pgm) => {
+  pgm.createTable("songs", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "TEXT",
+      notNull: true,
+    },
+    year: {
+      type: "INTEGER",
+      notNull: true,
+    },
+    genre: {
+      type: "TEXT",
+      notNull: true,
+    },
+    performer: {
+      type: "TEXT",
+      notNull: true,
+    },
+    duration: {
+      type: "INTEGER",
+    },
+    album_id: {
+      type: "VARCHAR(50)",
+    },
+  });
+
+  // Add foreign key constraint
+  pgm.addConstraint("songs", "fk_songs.album_id_albums.id", {
+    foreignKeys: {
+      columns: "album_id",
+      references: "albums(id)",
+      onDelete: "CASCADE",
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("songs");
+};
